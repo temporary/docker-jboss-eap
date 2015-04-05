8b9c4d5f2e1e244531c26a7e7bf7b25c2a3ed95e
temporary <temporary@github.com>
2015-04-05 19:18:29 +0200

    Configure Java Flight Recorder

--- 6.3.0/run.sh
+++ 6.3.0/run.sh
@@ -1,1 +1,3 @@
-docker run -p 9990:9990 -p 9999:9999 -p 8080:8080 -p 8787:8787 -it --rm temporary/jboss-eap:6.3.0
+mkdir jfr
+chmod o+w jfr
+docker run -p 9990:9990 -p 9999:9999 -p 8080:8080 -p 8787:8787 -v $(pwd)/jfr:/tmp/jboss/jfr -it --rm temporary/jboss-eap:6.3.0
